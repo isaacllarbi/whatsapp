d899939e8787ba8a218328f2db16634d475a0ea1
Isaac Larbi <isaacllarbi@gmailcom>
2020-06-09 03:13:39 +0000

    added google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:whatsapp/colors.dart';
 
 import 'screens/home/home_screen.dart';
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         primaryColorDark: primColorDark,
         accentColor: accentColor,
         buttonTheme: ButtonThemeData(buttonColor: accentColor),
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       initialRoute: HomeScreen.id,
       routes: {
